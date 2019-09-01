d0de9328e5d5e4661be6da79b1a6c47877a9da6a
alaa shaher <alaashaher12@gmail.com>
2019-09-01 14:29:54 +0200

    start home

---
+++ javascript/login.js
@@ -0,0 +1,15 @@
+var users = [
+    { id: 1, Email: "alaa@gmail.com", password: "123456" },
+    { id: 2, Email: "mohammed@gmail.com", password: "013579" }
+];
+function logIn () {            
+    let userEntered = document.getElementById("user").value;
+    let passwordEntered = document.getElementById("password").value;
+    let FilterArray = users.filter(user => user.Email.indexOf(userEntered) > -1);
+    if (passwordEntered == FilterArray[0].password) {
+        window.location = './html/admin.html';  
+        console.log(passwordEntered);
+    } else {
+        alert("email or password not correct");
+    }
+}
